f713ea28b7e8688e8805c7c8e7d10a011a5b05db
versluisant <rackipava@gmail.com>
2016-11-09 12:43:43 +0200

    ыефке

---
+++ code/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './app.hero-detail.component'
+
+@NgModule({
+  imports:      
+    [ 
+      BrowserModule, 
+      FormsModule 
+    ],
+  declarations: [ AppComponent, HeroDetailComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
