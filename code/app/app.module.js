1578ee651a3d2e9a06c394b62862826958922494
versluisant <rackipava@gmail.com>
2016-11-12 12:24:54 +0200

    выпаыв

--- code/app/app.module.js
+++ code/app/app.module.js
@@ -10,8 +10,9 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
-var app_component_1 = require('./app.component');
 var forms_1 = require('@angular/forms');
+var app_component_1 = require('./app.component');
+var my_hero_component_1 = require('./my-hero.component');
 var app_hero_detail_component_1 = require('./app.hero-detail.component');
 var AppModule = (function () {
     function AppModule() {
@@ -22,7 +23,7 @@ var AppModule = (function () {
                 platform_browser_1.BrowserModule,
                 forms_1.FormsModule
             ],
-            declarations: [app_component_1.AppComponent, app_hero_detail_component_1.HeroDetailComponent],
+            declarations: [app_component_1.AppComponent, app_hero_detail_component_1.HeroDetailComponent, my_hero_component_1.MyHeroComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
--- code/app/app.module.ts
+++ code/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MyHeroComponent } from './my-hero.component'
 import { HeroDetailComponent } from './app.hero-detail.component'
 
+
 @NgModule({
-  imports:      
-    [ 
-      BrowserModule, 
-      FormsModule 
-    ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:
+  [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [AppComponent, HeroDetailComponent, MyHeroComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
